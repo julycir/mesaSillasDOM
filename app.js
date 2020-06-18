a94bf16e6b76f09fff0a125c228a5da3b8c47849
Julieta Ciraolo <julycir@gmail.com>
2020-06-18 00:31:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+/*Crear una aplicación web que inicialmente muestre una mesa vacía. Utilizando el tablero
+de control (un botón de agregar y uno de borrar) se podrán agregar y borrar, de a una, las
+sillas que rodean la mesa. La mesa soporta hasta 8 mesas.*/
+
+window.onload = function() {
+
+	const sillas = document.querySelectorAll('.chair')
+	/*console.log(sillas)*/
+
+	const todasLasSillas = Array.from(sillas)
+	/*console.log(todasLasSillas)*/
+
+	for (let i = 0; i < todasLasSillas.length; i++) {
+		todasLasSillas[i].style.visibility = 'hidden'
+	}
+
+	const botonAgregar = document.querySelector('.addChair')
+
+    function agregarSilla() {
+    	todasLasSillas.forEach(function(item) {
+    	item.style.visibility ='visible'
+    	})
+    }
+
+	botonAgregar.addEventListener('click', agregarSilla)	
+
+	const botonRemover = document.querySelector('.removeChair')
+
+	function sacarSilla() {
+  		todasLasSillas.forEach(function(item) {
+    	item.style.visibility ='hidden'
+    	})
+    }
+
+	botonRemover.addEventListener('click', sacarSilla)
+
+}
